5587f0f2c77a7bfd8054de3f56b7ac4044f2d701
Akuzike8 <67435111+Akuzike8@users.noreply.github.com>
2023-08-25 01:20:57 +0200

    Add files via upload

---
+++ static/javascript/code_scanner.js
@@ -0,0 +1,46 @@
+const scan_btn = document.getElementById("scan-btn");
+const prompt_text = document.getElementById("card-prompt-text");
+const fileInput = document.getElementById("qr-code");
+
+scan_btn.addEventListener('click', () => {
+    fileInput.click();
+})
+
+function fetchRequest(file, formData) {
+    prompt_text.innerText = "Scanning QR Code...";
+    prompt_text.style = "color: black";
+    fetch("https://api.qrserver.com/v1/read-qr-code/", {
+        method: 'POST', body: formData
+    }).then(res => res.json()).then(result => {
+        result = result[0].symbol[0].data;
+        prompt_text.innerText = result ? "Please scan your qr card" : "Couldn't scan QR Code";
+        prompt_text.style = result ? "color: black;" : "color: red";
+        if(!result) return;
+        result = JSON.stringify(result)
+        console.log(result)
+        fetch("http://127.0.0.1:5000/scan_card", {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: result
+        })
+        .then(res => res.json())
+        .then(res => {
+            console.log(res)
+            alert(res.nid)
+        })
+        .catch((err) => {alert(err.message)});
+        //location.href = 'Fingerprint_auth.html';
+    }).catch(() => {
+        prompt_text.innerText = "Couldn't scan QR Code";
+    });
+}
+
+fileInput.addEventListener("change", async e => {
+    let file = e.target.files[0];
+    if(!file) return;
+    let formData = new FormData();
+    formData.append('file', file);
+    fetchRequest(file, formData);
+});
